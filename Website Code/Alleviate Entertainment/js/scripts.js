1d59fb87bbda2a11d1860b68ff8c9459a0bf2661
MarkusDielissen <92769885+MarkusDielissen@users.noreply.github.com>
2021-11-10 17:04:32 -0700

    Add files via upload

---
+++ Website Code/Alleviate Entertainment/js/scripts.js
@@ -0,0 +1,18 @@
+/*!
+* Start Bootstrap - Landing Page v6.0.4 (https://startbootstrap.com/theme/landing-page)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-landing-page/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+const checkpoint = 300;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= checkpoint) {
+    opacity = 1 - currentScroll / checkpoint;
+  } else {
+    opacity = 0;
+  }
+  document.querySelector(".front").style.opacity = opacity;
+});
